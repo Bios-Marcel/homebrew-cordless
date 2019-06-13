ad0407f935cb36ed8e13bd6c743a5e6ba1c513db
Marcel Schramm <marceloschr@googlemail.com>
2019-06-13 13:50:49 +0200

    Update to 2019-06-13

--- cordless.rb
+++ cordless.rb
@@ -1,9 +1,9 @@
 class Cordless < Formula
     desc "Discord, but in 1984"
     homepage "https://github.com/Bios-Marcel/cordless"
-    url "https://github.com/Bios-Marcel/cordless/archive/2019-05-14.tar.gz"
-    version "2019-05-14"
-    sha256 "af17df435d2cb177818e74db677d020480797241f55de978280aa39d6b7ca36e"
+    url "https://github.com/Bios-Marcel/cordless/archive/2019-06-13.tar.gz"
+    version "2019-06-13"
+    sha256 "ef9c4db21b7c75a5263281ed783c905b47e752f8b596575e0a41cd6e7d450566"
     head "https://github.com/Bios-Marcel/cordless.git"
   
     depends_on "go" => :build
