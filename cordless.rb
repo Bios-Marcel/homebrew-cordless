434e9baf24beeffb224787d85ed2303061d96df3
Marcel Schramm <marceloschr@googlemail.com>
2020-06-29 23:50:08 +0200

    download link was incorrect

--- cordless.rb
+++ cordless.rb
@@ -1,7 +1,7 @@
 class Cordless < Formula
     desc "Discord, but in 1984"
     homepage "https://github.com/Bios-Marcel/cordless"
-    url "https://github.com/Bios-Marcel/cordless/archive/.tar.gz"
+    url "https://github.com/Bios-Marcel/cordless/archive/2020-06-26.tar.gz"
     version "2020-06-26"
     sha256 "9ccec588519ffaaaf869f519223c8cb38f22437e38829c891c32f7d42af97f96"
     head "https://github.com/Bios-Marcel/cordless.git"
